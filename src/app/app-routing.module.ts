14cd4a0d378b86edecb622615ae506d26c813b0a
Amira Salah <amirasalah@Amiras-MacBook-Pro.local>
2019-02-01 23:47:41 +0200

    Adding structure

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RandomQuoteComponent } from '../app/Random Quote/random-quote.component';
+import { AvailableCategoriesComponent } from '../app/Available Categories/available-categories.component';
+import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent  },
+  { path: 'random', component: RandomQuoteComponent  },
+  { path: 'categories', component: AvailableCategoriesComponent },
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { RandomQuoteComponent } from './random-quote/random-quote.component';
-import { AvailableCategoriesComponent } from './available-categories/available-categories.component';
+import { RandomQuoteComponent } from '../app/Random Quote/random-quote.component';
+import { AvailableCategoriesComponent } from '../app/Available Categories/available-categories.component';
 import { HeaderComponent } from './header/header.component';
-import { TwitterRelatedComponent } from './twitter-related/twitter-related.component';
-import { GiphyRelatedComponent } from './giphy-related/giphy-related.component';
+import { TwitterRelatedComponent } from '../app/Social Components/Twitter Related/twitter-related.component';
+import { GiphyRelatedComponent } from '../app/Social Components/Giphy Related/giphy-related.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +17,8 @@ import { GiphyRelatedComponent } from './giphy-related/giphy-related.component';
     AvailableCategoriesComponent,
     HeaderComponent,
     TwitterRelatedComponent,
-    GiphyRelatedComponent
+    GiphyRelatedComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
