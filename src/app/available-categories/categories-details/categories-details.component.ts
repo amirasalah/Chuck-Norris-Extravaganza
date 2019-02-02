fe96c8139daef90ac9c188c4eaae5e4eece8b2c9
Amira Salah <amirasalah@Amiras-MacBook-Pro.local>
2019-02-02 18:09:11 +0200

    Finalize App

--- src/app/available-categories/categories-details/categories-details.component.ts
+++ src/app/available-categories/categories-details/categories-details.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
-import { QuoteGeneratorService } from '../../services/quote-generator-api/quote-generator.service';
+import { QuoteGeneratorService } from "../../services/quote-generator-api/quote-generator.service";
 @Component({
   selector: "app-categories-details",
   templateUrl: "./categories-details.component.html",
@@ -10,18 +10,23 @@ export class CategoriesDetailsComponent implements OnInit {
   public searchWord;
   public searchResults;
   constructor(
-    private quoteGeneratorService:QuoteGeneratorService,
-    private activeRoute: ActivatedRoute,
+    private quoteGeneratorService: QuoteGeneratorService,
+    private activeRoute: ActivatedRoute
   ) {}
-
+  colors = ["#DDDCDD", "#E8CA7D", "#A37D4E", "#D29A6A", "#F2DBC3" , "#54758F" , "#DDC0AE" , " #787A8E" , "#B3B9C4" ];
+  generateBackground(){
+    const x = this.colors.length;
+    const rand = Math.floor(Math.random()*x) + 1;
+    return this.colors[rand];
+  }
   ngOnInit() {
     this.activeRoute.params.subscribe(routeParams => {
       this.searchWord = routeParams;
-      console.log(this.searchWord.category);
-      this.quoteGeneratorService.searchCategory(this.searchWord.category).subscribe(res => {
-        this.searchResults = res;
-      })
-
+      this.quoteGeneratorService
+        .searchCategory(this.searchWord.category)
+        .subscribe(res => {
+          this.searchResults = res;
+        });
     });
   }
 }
