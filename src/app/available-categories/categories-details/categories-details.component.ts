6839e6863bfcda1bf5896e687f3f5d0496df5463
Amira Salah <Amira.Salah@itworx.com>
2019-02-05 15:35:49 +0200

    fixing issues

--- src/app/available-categories/categories-details/categories-details.component.ts
+++ src/app/available-categories/categories-details/categories-details.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, OnInit, ChangeDetectorRef } from "@angular/core";
 import { ActivatedRoute } from "@angular/router";
 import { QuoteGeneratorService } from "../../services/quote-generator-api/quote-generator.service";
 @Component({
@@ -7,17 +7,33 @@ import { QuoteGeneratorService } from "../../services/quote-generator-api/quote-
   styleUrls: ["./categories-details.component.sass"]
 })
 export class CategoriesDetailsComponent implements OnInit {
-  public searchWord;
-  public searchResults;
+  public searchWord:string;
+  public searchResults:[];
   constructor(
+    private cdr: ChangeDetectorRef,
     private quoteGeneratorService: QuoteGeneratorService,
     private activeRoute: ActivatedRoute
   ) {}
-  colors = ["#DDDCDD", "#E8CA7D", "#A37D4E", "#D29A6A", "#F2DBC3" , "#54758F" , "#DDC0AE" , " #787A8E" , "#B3B9C4" ];
-  generateBackground(){
-    const x = this.colors.length;
-    const rand = Math.floor(Math.random()*x) + 1;
-    return this.colors[rand];
+  public colors = [
+    "#DDDCDD",
+    "#E8CA7D",
+    "#A37D4E",
+    "#D29A6A",
+    "#F2DBC3",
+    "#54758F",
+    "#DDC0AE",
+    "#787A8E",
+    "#B3B9C4"
+  ];
+  public arrayLength:number = this.colors.length;
+  generateBackground(index) {
+    console.log(index)
+    if(index <=5){
+      const rand = Math.floor(Math.random() * (this.arrayLength - 1)) + 1;
+      console.log(rand, this.colors[rand]);
+      return this.colors[rand];
+    }
+    
   }
   ngOnInit() {
     this.activeRoute.params.subscribe(routeParams => {
@@ -27,6 +43,10 @@ export class CategoriesDetailsComponent implements OnInit {
         .subscribe(res => {
           this.searchResults = res;
         });
-    });
+    }).filter((el, index)=> { });
+  }
+  ngAfterViewInit() {
+    console.log(this.cdr.detectChanges())
+    this.cdr.detectChanges();
   }
 }
